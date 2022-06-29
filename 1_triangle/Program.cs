6853fc413a2f36b65477968704ffd0a684951b8f
geekbrains <info@geekbrains.ru>
2022-06-29 19:19:17 +0400

    triangle

---
+++ 1_triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿int a = InputData("Input side a: ");
+int b = InputData("Input side b: ");
+int c = InputData("Input side c: ");
+int min1 = a;
+int min2 = b;
+int max = c;
+
+if(a == b && b == c && a == c)
+{
+    Console.Write("Yes, with that sides is possible an equilateral triangle");   
+}
+else
+{
+    if(a >= b && a >= c)
+    {
+        max = a;
+        min1 = b;
+        min2 = c;
+    }    
+    if(b >= a && b >= c)
+    {
+        max = b;
+        min1 = a;
+        min2 = c;
+    }
+
+    if(max < min1 + min2)
+    {
+        Console.Write("Yes, triangle is possible");
+    }
+    else
+    {
+        Console.Write("No, triangle is impossible");
+    }
+}
+
+
+int InputData(string output)
+{
+    Console.Write(output);
+    int inpData = Convert.ToInt32(Console.ReadLine());
+    return inpData;
+}
